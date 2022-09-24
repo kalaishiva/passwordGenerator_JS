f194c9b4f5ac9dbf28f9b7edfbe88b33bd044d1d
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-09-24 21:47:15 +0200

    some function prepared

--- script.js
+++ script.js
@@ -7,11 +7,21 @@ const symbolsEl = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
 const clipboardEl = document.getElementById('clipboard')
 
+//result
+console.log(resultEl);
+
 
 //password length
 const passLength = lengthEl.value;
 
-
+//letter
+const letter = "abcdefghijklmnopqrstuvwxyz";
+let number = "0123456789";
+/* let number1 = number.split("");
+console.log(number1); */
+let symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+let symbol = symbols.split(""); //length of the symbol array
+//console.log(symbol);
 
 
 const randomFunc = {
@@ -33,10 +43,34 @@ function generatePassword(lower, upper, number, symbol, length) {
 
 }
 
-function getRandomLower() {}
+function getRandomLower() {
+    //let lowerC = lowercaseEl.value;
+    //console.log(lowerC);
+    lowerC = letter[(Math.floor(Math.random() * 26))];
+    resultEl.innerHTML = lowerC.toLowerCase();
 
-function getRandomUpper() {}
+}
 
-function getRandomNumber() {}
+function getRandomUpper() {
+    let lowerC = lowercaseEl.value;
+    lowerC = letter[(Math.floor(Math.random() * 26))];
+    resultEl.innerHTML = lowerC.toUpperCase();
 
-function getRandomSymbol() {}
+}
+
+function getRandomNumber() {
+    let randomNumber = number[(Math.floor(Math.random() * 10))];
+    resultEl.innerHTML = randomNumber;
+
+}
+
+function getRandomSymbol() {
+    let randomSymbol = symbol[(Math.floor(Math.random() * (symbol.length)))];
+    resultEl.innerHTML = randomSymbol;
+
+}
+
+getRandomLower();
+getRandomUpper();
+getRandomNumber();
+getRandomSymbol();
